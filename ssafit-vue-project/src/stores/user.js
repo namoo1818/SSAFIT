9b1bdc4c187d8aa86137a3bd54c0e8b7e3c09d29
ahyeongcho <aystarr@naver.com>
2023-11-21 13:50:08 +0900

    commit

--- ssafit-vue-project/src/stores/user.js
+++ ssafit-vue-project/src/stores/user.js
@@ -8,27 +8,26 @@ const REST_FOLLOW_API = `http://localhost:8080/follow`
 
 export const useUserStore = defineStore('user',()=>{
 
-    //필요한 거면 주석 해제 ㄱㄱ 
-    const loginUserId = ref('')
-    const userLogin = function(id, pw){
-
-        axios.post(`${REST_USER_API}/login`,{userId: id, userPassword: pw})
-        .then((response)=>{
-            sessionStorage.setItem('access-token', response.data["access-token"])
-            
-            const token = response.data['access-token'].split('.')
-            let id = token[1]
-            id = atob(id)
-            id = JSON.parse(id)
-            console.log(id)
-            loginUserId.value = id['id']
-
-            router.push({name:"video"})
-        })
-        .catch(()=>{
-
-        })
-    }
+    //작동이 안됨 
+    // const login = function(id, pw){
+    //     axios.post(`${REST_USER_API}/login`,{userId: id, userPassword: pw})
+    //     .then((response)=>{
+    //         let matchedUser = res.data.find(
+    //             (u) => u.id === login.id && u.password === login.password
+    //         );
+    //         if (matchedUser) {
+    //             user.value = matchedUser;
+    //             localStorage.setItem("loginUser",Json.stringify(matchedUser))
+    //             alert("로그인에 성공하였습니다.");
+    //         } else {
+    //             alert("아이디 또는 비밀번호가 맞지 않습니다.");
+    //         }
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //         alert("에러가 발생하였습니다.");
+    //     })
+    // }
 
     // 사용자 전체
     const userList = ref([])
@@ -139,7 +138,7 @@ export const useUserStore = defineStore('user',()=>{
     })
       }
 
-      return {userList, loginUserId, userLogin, getUserList, user, getUser, createUser, updateUser, deleteUser,
+      return { userList, getUserList, user, getUser, createUser, updateUser, deleteUser,
         getWishList, getFollowerList, getFollowingList,
         wishList, followerList, followingList, getMyReviewList, myReviewList}
     
